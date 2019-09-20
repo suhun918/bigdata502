be90b96d17d22e1b705fb6b4f2cb8090ba49f165
Suheon <suhun918@nate.com>
2019-09-20 12:14:38 +0900

    함수 추가

--- Animal.java
+++ Animal.java
@@ -1,5 +1,9 @@
 
 public class Animal {
+	
+	public void play1() {
+		System.out.println("гого");
+	}
 
 	public void play() {
 		System.out.println("╦ш╦ш");
@@ -7,6 +11,7 @@ public class Animal {
 	public static void main(String[] args) {
 		Animal a = new Animal();
 		a.play();
+		a.play1();
 
 	}
 
